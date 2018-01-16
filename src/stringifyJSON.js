c70b704b5cfb70522bdc3fbdce28b4b9249c9e55
jqywang <jqywang.95@gmail.com>
2018-01-15 16:00:29 -0800

    "stringifyJSON now passes all tests"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,33 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  //if (is array)
+  //add brackets, pass inside of array into funciton again
+  //if object
+  //add curly brackets, set 'key' + ':' + 'value put into function again'
+  // add '"' to it
+
+  if (Array.isArray(obj)) {
+    var arrayString = [];
+    for (var i = 0; i < obj.length; i++) {
+      arrayString.push(stringifyJSON(obj[i]));
+    }
+    arrayString = arrayString.join(',');
+    return '[' + arrayString + ']';
+  } else if (typeof obj === 'object' && obj != null) {
+    var objString = [];
+    for (var key in obj) {
+      if (typeof obj[key] !== 'function' && obj[key] !== undefined) {
+        var keyValueString = stringifyJSON(key) + ':' + stringifyJSON(obj[key]);
+        objString.push(keyValueString);
+      }
+    }
+    objString = objString.join(',');
+    return '{' + objString + '}';
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (typeof obj !== 'function' || obj !== undefined) {
+    return '' + obj;
+  }
+  
 };
