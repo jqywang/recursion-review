a400dd1194afc413925d119d8f45bede7bc60179
jqywang <jqywang.95@gmail.com>
2018-01-15 16:00:03 -0800

    "getElementsByClassName passes all tests"

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,25 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className, body) {
+  var allClasses = [];
+  if (body === undefined) {
+    body = document.body;
+  }
+  if (body.classList) {
+    if (body.classList.contains(className)) {
+      allClasses.push(body);
+    }
+  }
+  if (body.hasChildNodes()) {
+    //iterate through childnodes
+    var children = body.childNodes;
+    console.log(children[0]);
+    for (var i = 0; i < children.length; i ++) {
+      allClasses = allClasses.concat(getElementsByClassName(className, children[i]));
+    }
+  }
   // your code here
+  // use document.body, elements.classlist, element.childnodes
+  return allClasses;
 };
